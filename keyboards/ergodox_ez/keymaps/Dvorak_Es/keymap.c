5ffa03e7d5f9c3da3fbdb0671984433cc87b097e
David Hayden <dhayden@talosthoren.com>
2025-08-11 16:52:17 -0300

    Add ndash/mdash unicode.

--- keyboards/ergodox_ez/keymaps/Dvorak_Es/keymap.c
+++ keyboards/ergodox_ez/keymaps/Dvorak_Es/keymap.c
@@ -15,11 +15,15 @@ enum custom_keycodes {
 enum unicode_names {
     U_QTBL,
     U_QTBR,
+    U_NDSH,
+    U_MDSH,
 };
 
 const uint32_t unicode_map[] PROGMEM = {
     [U_QTBL] = 0x00AB,
     [U_QTBR] = 0x00BB,
+    [U_NDSH] = 0x2013,
+    [U_MDSH] = 0x2014,
 };
 
 // clang-format off
@@ -29,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |    º   |   1  |   2  |   3  |   4  |   5  | ACC  |           | IEXP |   6  |   7  |   8  |   9  |   0  | CapsLk |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   .  |   ,  |   Ñ  |   P  |   Y  | HOME |           | PGUP |   F  |   G  |   C  |   R  |   L  |   /    |
+ * | Tab    |   .  |   ,  |   Ñ  |   P  |   Y  | HOME |           | PGUP |   F  |   G  |   C  |   R  |   L  |   –    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ESC    |   A  |   O  |   E  |   U  |   I  |------|           |------|   D  |   H  |   T  |   N  |   S  |   -    |
  * |--------+------+------+------+------+------| END  |           | PGDN |------+------+------+------+------+--------|
@@ -48,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
   ES_MORD,         ES_1,        ES_2,          ES_3,    ES_4,    ES_5,    ES_ACUT,              ES_IEXL,      ES_6,    ES_7,    ES_8,    ES_9,    ES_0,    KC_CAPS,
-  KC_TAB,          ES_DOT,      ES_COMM,       ES_NTIL, ES_P,    ES_Y,    KC_HOME,              KC_PGUP,      ES_F,    ES_G,    ES_C,    ES_R,    ES_L,    ES_SLSH,
+  KC_TAB,          ES_DOT,      ES_COMM,       ES_NTIL, ES_P,    ES_Y,    KC_HOME,              KC_PGUP,      ES_F,    ES_G,    ES_C,    ES_R,    ES_L,    UP(U_NDSH,U_MDSH),
   KC_ESC,          ES_A,        ES_O,          ES_E,    ES_U,    ES_I,                                        ES_D,    ES_H,    ES_T,    ES_N,    ES_S,    ES_MINS,
   KC_LSFT,         ES_LABK,     ES_Q,          ES_J,    ES_K,    ES_X,    KC_END,               KC_PGDN,      ES_B,    ES_M,    ES_W,    ES_V,    ES_Z,    KC_RSFT,
   KC_ALGR,         ES_PLUS, UP(U_QTBL,U_QTBR), KC_LEFT, KC_RGHT,                                                       KC_UP,   KC_DOWN, ES_QUOT, ES_GRV,  KC_ALGR,
@@ -62,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |    º   |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           | F12  |  F6  |  F7  |  F8  |  F9  | F10  | CapsLk |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   .  |   ,  |   Ñ  |   P  |   Y  | HOME |           | PGUP |   F  |   G  |   C  |   R  |   L  |   /    |
+ * | Tab    |   .  |   ,  |   Ñ  |   P  |   Y  | HOME |           | PGUP |   F  |   G  |   C  |   R  |   L  |   –    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ESC    |   A  |   O  |   E  |   U  |   I  |------|           |------|   D  |   H  |   T  |   N  |   S  |   -    |
  * |--------+------+------+------+------+------| END  |           | PGDN |------+------+------+------+------+--------|
@@ -81,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [FUNC] = LAYOUT_ergodox_pretty(
   // left hand
   ES_MORD,         KC_F1,       KC_F2,         KC_F3,   KC_F4,   KC_F5,   KC_F11,               KC_F12,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_CAPS,
-  KC_TAB,          ES_DOT,      ES_COMM,       ES_NTIL, ES_P,    ES_Y,    KC_HOME,              KC_PGUP,      ES_F,    ES_G,    ES_C,    ES_R,    ES_L,    ES_SLSH,
+  KC_TAB,          ES_DOT,      ES_COMM,       ES_NTIL, ES_P,    ES_Y,    KC_HOME,              KC_PGUP,      ES_F,    ES_G,    ES_C,    ES_R,    ES_L,    UP(U_NDSH,U_MDSH),
   KC_ESC,          ES_A,        ES_O,          ES_E,    ES_U,    ES_I,                                        ES_D,    ES_H,    ES_T,    ES_N,    ES_S,    ES_MINS,
   KC_LSFT,         ES_LABK,     ES_Q,          ES_J,    ES_K,    ES_X,    KC_END,               KC_PGDN,      ES_B,    ES_M,    ES_W,    ES_V,    ES_Z,    KC_RSFT,
   KC_ALGR,         ES_PLUS, UP(U_QTBL,U_QTBR), KC_LEFT, KC_RGHT,                                                       KC_UP,   KC_DOWN, ES_QUOT, ES_GRV,  KC_ALGR,
@@ -95,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |    º   |   1  |   2  |   3  |   4  |   5  | PLAY |           | MUTE |   6  | NMLK |   /  |   *  |   -  | CapsLk |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   .  |   ,  |   Ñ  |   P  |   Y  | NEXT |           | VOLU |   F  |   7  |   8  |   9  |   +  |   /    |
+ * | Tab    |   .  |   ,  |   Ñ  |   P  |   Y  | NEXT |           | VOLU |   F  |   7  |   8  |   9  |   +  |   –    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ESC    |   A  |   O  |   E  |   U  |   I  |------|           |------|   D  |   4  |   5  |   6  |   =  |   -    |
  * |--------+------+------+------+------+------| PREV |           | VOLD |------+------+------+------+------+--------|
@@ -114,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NUMPAD] = LAYOUT_ergodox_pretty(
   // left hand
   ES_MORD,         ES_1,        ES_2,          ES_3,    ES_4,    ES_5,    KC_MPLY,              KC_MUTE,      ES_6,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, KC_CAPS,
-  KC_TAB,          ES_DOT,      ES_COMM,       ES_NTIL, ES_P,    ES_Y,    KC_MNXT,              KC_VOLU,      ES_F,    KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, ES_SLSH,
+  KC_TAB,          ES_DOT,      ES_COMM,       ES_NTIL, ES_P,    ES_Y,    KC_MNXT,              KC_VOLU,      ES_F,    KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, UP(U_NDSH,U_MDSH),
   KC_ESC,          ES_A,        ES_O,          ES_E,    ES_U,    ES_I,                                        ES_D,    KC_KP_4, KC_KP_5, KC_KP_6, KC_PEQL, ES_MINS,
   KC_LSFT,         ES_LABK,     ES_Q,          ES_J,    ES_K,    ES_X,    KC_MPRV,              KC_VOLD,      ES_B,    KC_KP_1, KC_KP_2, KC_KP_3, KC_PDOT, KC_RSFT,
   KC_ALGR,         ES_PLUS, UP(U_QTBL,U_QTBR), KC_LEFT, KC_RGHT,                                                       KC_UP,   KC_DOWN, KC_KP_0, ES_GRV,  KC_ALGR,
